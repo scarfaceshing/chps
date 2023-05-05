6c6822b2d329b5659b17347eaa657921ff67ded8
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2023-05-05 09:18:31 +0800

    first commit

---
+++ login.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+?>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Login | CPSU HINIGARAN POLLING SYSTEM</title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+<?php 
+if(isset($_SESSION['create_id']))
+header("location:index.php?page=home");
+
+?>
+
+</head>
+<style>
+	body{
+		background-image: url('cpsu_lab.jpg'); 
+  background-color: #cccccc;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+		width: 100%;
+	    position: fixed;
+
+	    /*background: yellowgreen;*/
+	}
+	main#main{
+		width:100%;
+		height: calc(100%);
+		display: flex;
+	}
+
+</style>
+
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+  position: relative;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 25px;
+  top: 0;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: red;
+  cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
+
+<body style="background-image: url('cpsubuilding.jpg');">
+
+
+  <main id="main" >
+  	
+  		<div class="align-self-center w-100">
+		
+  		<div id="login-center" class="bg-hsl(120, 60%, 70%); row justify-content-center">
+  			<div class="card col-md-4">
+  				<div class="card-body">
+				  <h4 class="text-dark text-center"><b> CPSU HINIGARAN POLLING SYSTEM</b></h4>
+  					<form id="login-form" >
+  						<div class="form-group">
+  							<label for="email" class="control-label text-dark">Email</label>
+  							<input type="text" id="email" name="email" class="form-control form-control-sm">
+  						</div>
+  						<div class="form-group">
+  							<label for="password" class="control-label text-dark">Password</label>
+  							<input type="password" id="password" name="password" class="form-control form-control-sm">
+  						</div>
+  						<center><button class=" btn-block btn-wave col-md-4 btn-primary" type="login">Login</button></center><br>
+						 
+  					</form>
+					  <center><button onclick="document.getElementById('id01').style.display='block'" style="width:auto; col-md-4" class="btn-danger">Create Account</button></center>
+  				</div>
+  			</div>
+  		</div>
+  		</div>
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="" id="manage_user">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      
+    </div>
+
+    <div class="container">
+		<div class="container-body">
+			<form action="" id="manage_user">
+				<input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
+				<div class="row">
+					<div class="col-md-6 border-right">
+						<b class="text-muted">Personal Information</b>
+						<div class="form-group">
+							<label for="" class="control-label text-dark">First Name</label>
+							<input type="text" name="firstname" class="form-control form-control-sm" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+						</div>
+						<div class="form-group">
+							<label for="" class="control-label text-dark">Middle Name</label>
+							<input type="text" name="middlename" class="form-control form-control-sm"  value="<?php echo isset($middlename) ? $middlename : '' ?>">
+						</div>
+						<div class="form-group">
+							<label for="" class="control-label text-dark">Last Name</label>
+							<input type="text" name="lastname" class="form-control form-control-sm" required value="<?php echo isset($lastname) ? $lastname : '' ?>">
+						</div>
+						<div class="form-group">
+							<label for="" class="control-label text-dark">Contact No.</label>
+							<input type="text" name="contact" class="form-control form-control-sm" required value="<?php echo isset($contact) ? $contact : '' ?>">
+						</div>
+						<div class="form-group">
+							<label class="control-label text-dark">Address</label>
+							<textarea name="address" id="" cols="30" rows="4" class="form-control" required><?php echo isset($address) ? $address : '' ?></textarea>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<b class="text-muted">System Credentials</b>
+						
+						<div class="form-group">
+							<label for="" class="control-label">User Role</label>
+							<select name="type" id="type" class="custom-select custom-select-sm">
+								<option value="3" <?php echo isset($type) && $type == 3 ? 'selected' : '' ?>>Subscriber</option>
+								<option value="2" <?php echo isset($type) && $type == 2 ? 'selected' : '' ?>>Staff</option>
+								<option value="1" <?php echo isset($type) && $type == 1 ? 'selected' : '' ?>>Admin</option>
+							</select>
+						</div>
+						
+							<input type="hidden" name="type" value="3">
+						
+						<div class="form-group">
+							<label class="control-label text-dark">Email</label>
+							<input type="email" class="form-control form-control-sm" name="email" required value="<?php echo isset($email) ? $email : '' ?>">
+							<small id="#msg"></small>
+						</div>
+						<div class="form-group">
+							<label class="control-label text-dark">Password</label>
+							<input type="password" class="form-control form-control-sm" name="password" <?php echo isset($id) ? "":'required' ?>>
+							<small><i><?php echo isset($id) ? "Leave this blank if you dont want to change you password":'' ?></i></small>
+						</div>
+						<div class="form-group">
+							<label class="label control-label text-dark">Confirm Password</label>
+							<input type="password" class="form-control form-control-sm" name="cpass" <?php echo isset($id) ? 'required' : '' ?>>
+							<small id="pass_match" data-status=''></small>
+						</div>
+					</div>
+				</div>
+
+    <div class="container" style="background-color:#f1f1f1">
+	<center><button class="btn-block btn-wave btn-primary" style="width:auto;">Save</button></center>
+      <center><button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button></center>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+</body>
+
+
+</body>
+<script>
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form button[type="login"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="login"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Error Try Again.</div>')
+					$('#login-form button[type="login"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+	<script>
+	$('[name="password"],[name="cpass"]').keyup(function(){
+		var pass = $('[name="password"]').val()
+		var cpass = $('[name="cpass"]').val()
+		if(cpass == '' ||pass == ''){
+			$('#pass_match').attr('data-status','')
+		}else{
+			if(cpass == pass){
+				$('#pass_match').attr('data-status','1').html('<i class="text-success">Password Matched.</i>')
+			}else{
+				$('#pass_match').attr('data-status','2').html('<i class="text-danger">Password does not match.</i>')
+			}
+		}
+	})
+	function displayImg(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
+	$('#manage_user').submit(function(e){
+		e.preventDefault()
+		$('input').removeClass("border-danger")
+		start_load()
+		$('#msg').html('')
+		if($('#pass_match').attr('data-status') != 1){
+			if($("[name='password']").val() !=''){
+				$('[name="password"],[name="cpass"]').addClass("border-danger")
+				end_load()
+				return false;
+			}
+		}
+		$.ajax({
+			url:'ajax.php?action=save_user',
+			data: new FormData($(this)[0]),
+		    cache: false,
+		    contentType: false,
+		    processData: false,
+		    method: 'POST',
+		    type: 'POST',
+			success:function(resp){
+				if(resp == 1){
+					alert_toast('Data successfully saved.',"success");
+					setTimeout(function(){
+						location.replace('index.php?page=user_list')
+					},750)
+				}else if(resp == 2){
+					$('#msg').html("<div class='alert alert-danger'>Email already exist.</div>");
+					$('[name="email"]').addClass("border-danger")
+					end_load()
+				}
+			}
+		})
+	})
+</script>
+</html>
